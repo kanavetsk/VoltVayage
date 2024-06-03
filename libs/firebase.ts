637b0134646c0b76dc5f48af26e003c856f5c8fe
MacUser <macuser@MacBook-Air-MacUser.local>
2024-06-03 22:02:21 +0300

    Опис змін

--- libs/firebase.ts
+++ libs/firebase.ts
@@ -4,7 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {};
+const firebaseConfig = {
+    apiKey: "AIzaSyAZ7gaLftRgZY_O7IeW0EYvg8P8x9SvDtU",
+    authDomain: "e-shop-c6fce.firebaseapp.com",
+    projectId: "e-shop-c6fce",
+    storageBucket: "e-shop-c6fce.appspot.com",
+    messagingSenderId: "339567571100",
+    appId: "1:339567571100:web:b27b7c8c89797397e17d59"
+  };
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
